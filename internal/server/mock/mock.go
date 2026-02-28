030119012e5aee1a4d202ee95fd9cb3e33aa2a4c
Oleksii Troian <trolleksii@gmail.com>
2026-02-28 14:37:31 -0500

    mock endpoint to test without github webhook

---
+++ internal/server/mock/mock.go
@@ -0,0 +1,83 @@
+package mock
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/trolleksii/argocd-diff-reporter/internal/bus"
+	"github.com/trolleksii/argocd-diff-reporter/internal/config"
+	"github.com/trolleksii/argocd-diff-reporter/internal/server"
+)
+
+type mockPayload struct {
+	Action     string `json:"action"`
+	Repository string `json:"repository"`
+	Owner      string `json:"owner"`
+	PrNum      string `json:"prNum"`
+	Title      string `json:"title"`
+	Author     string `json:"author"`
+	Branch     string `json:"branch"`
+	BaseSha    string `json:"baseSha"`
+	HeadSha    string `json:"headSha"`
+}
+
+type MockHandler struct {
+	bus *bus.Bus
+	log *slog.Logger
+}
+
+func Route(cfg config.WebhookConfig, log *slog.Logger, b *bus.Bus) server.Route {
+	return func(mux *http.ServeMux) {
+		mux.Handle("/mock", newMockHandler(log, b))
+	}
+}
+
+func newMockHandler(log *slog.Logger, b *bus.Bus) *MockHandler {
+	return &MockHandler{
+		log: log.With("module", "server", "handler", "mock"),
+		bus: b,
+	}
+}
+
+func (h *MockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var payload mockPayload
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		h.log.Warn("Failed to parse mock payload", "error", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	switch payload.Action {
+	case "closed":
+		err := h.bus.Publish(r.Context(), bus.Message{
+			Subject: "pr.closed",
+			Headers: map[string]string{
+				"repository": payload.Repository,
+				"owner":      payload.Owner,
+				"prNum":      payload.PrNum,
+			},
+		})
+		if err != nil {
+			h.log.Error("Failed to publish PR event", "error", err)
+		}
+	case "opened", "synchronize", "reopened":
+		err := h.bus.Publish(r.Context(), bus.Message{
+			Subject: "pr.changed",
+			Headers: map[string]string{
+				"repository": payload.Repository,
+				"owner":      payload.Owner,
+				"prNum":      payload.PrNum,
+				"title":      payload.Title,
+				"author":     payload.Author,
+				"branch":     payload.Branch,
+				"baseSha":    payload.BaseSha,
+				"headSha":    payload.HeadSha,
+			},
+		})
+		if err != nil {
+			h.log.Error("Failed to publish PR event", "error", err)
+		}
+		h.log.Debug("new request enqued", "pr", payload.PrNum)
+	}
+}
