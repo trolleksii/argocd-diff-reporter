fb9d9a4ee4a0da46319bd69b565fd69bae5aa10c
Oleksii Troian <trolleksii@gmail.com>
2026-03-16 18:06:25 -0400

    format

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,8 +14,8 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 // Repository represents a git repository
